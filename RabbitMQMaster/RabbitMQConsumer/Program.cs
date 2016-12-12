ebc16f23b2a942311a7557023f8c7073b992a3ef
梁士伟 <deawback@hotmail.com>
2016-12-12 23:42:45 +0800

    添加了rabbitmq项目 发布与消费者,基本demo

---
+++ RabbitMQMaster/RabbitMQConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //consumber端
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "admin"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //获取消息
+                var result = channel.BasicGet("firstTest", true);
+
+                var msg = Encoding.UTF8.GetString(result.Body);
+                //打印消息
+                Console.WriteLine(msg);
+
+            }
+        }
+    }
+}
---
+++ RabbitMQMaster/RabbitMQServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //从工厂中拿到实例 本地host、用户admin
+            var factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                HostName = "localhost"
+            };
+
+            //创建连接
+            using (var connection = factory.CreateConnection())
+            //创建返回一个新的频道
+            using (var channel = connection.CreateModel())
+            {
+                //使用默认的交换机
+
+                //声明队列
+                channel.QueueDeclare("firstTest", true, false, false, null);
+                //发布消息
+                var msg = Encoding.UTF8.GetBytes("Hello RabbitMQ");
+                channel.BasicPublish(string.Empty, routingKey: "firstTest", basicProperties: null, body: msg);
+            }
+
+        }
+    }
+}
+    
